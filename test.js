9611c580e44b39220e071c6c923e21e0917706ba
Dawson Botsford <DawsonBotsford@gmail.com>
2016-03-16 10:25:55 -0600

    add blank line in test

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ import childProcess from 'child_process';
 import test from 'ava';
 import pify from 'pify';
 import logSymbols from 'log-symbols';
+
 test(async t => {
 	const rand = `asdasfgrgafadsgaf + ${Math.random().toString().slice(2)}`;
 	const stdout = await pify(childProcess.execFile)('./cli.js', [rand, '--color'], {cwd: __dirname});
