e6559e986acdf043c75f2c5a3cf10c2e39c04937
Jason Maurer <j@maur.co>
2016-04-07 00:51:08 -0600

    implemented .many function

--- cli.js
+++ cli.js
@@ -7,16 +7,16 @@ const npmName = require('npm-name');
 
 const cli = meow(`
 	Usage
-	  $ npm-name <name>
+		$ npm-name <name>
 
 	Examples
-	  $ npm-name chalk
-	  ${logSymbols.error} ${chalk.bold('chalk')} is unavailable
-	  $ npm-name unicorn-cake
-	  ${logSymbols.success} ${chalk.bold('unicorn-cake')} is available
-	  $ npm-name chalk unicorn-cake
-	  ${logSymbols.error} ${chalk.bold('chalk')} is unavailable
-	  ${logSymbols.success} ${chalk.bold('unicorn-cake')} is available
+		$ npm-name chalk
+		${logSymbols.error} ${chalk.bold('chalk')} is unavailable
+		$ npm-name unicorn-cake
+		${logSymbols.success} ${chalk.bold('unicorn-cake')} is available
+		$ npm-name chalk unicorn-cake
+		${logSymbols.error} ${chalk.bold('chalk')} is unavailable
+		${logSymbols.success} ${chalk.bold('unicorn-cake')} is available
 
 	Exits with code 0 when the name is available or 2 when taken
 `, {
@@ -30,10 +30,10 @@ if (!input) {
 	process.exit(1);
 }
 
-npmName(input).then(available => {
-  input.forEach((name, indx) => {
-    outputStatus(name, available[indx]);
-  });
+npmName.many(input).then(available => {
+	input.forEach((name, indx) => {
+		outputStatus(name, available[indx]);
+	});
 
 	process.exit(available.indexOf(false) > -1 ? 2 : 0);
 });
--- test.js
+++ test.js
@@ -13,17 +13,17 @@ test(async t => {
 		const ret = await execa('./cli.js', [pkgName, '--color'], {cwd: __dirname});
 		t.regex(ret.stdout, /is unavailable/);
 	} catch (err) {
-		t.ok(err)
+		t.ok(err);
 	}
 });
 
 test(async t => {
-	const pkgName1 = 'chalk'
-	const pkgName2 = `asdasfgrgafadsgaf${Math.random().toString().slice(2)}`
+	const pkgName1 = 'chalk';
+	const pkgName2 = `asdasfgrgafadsgaf${Math.random().toString().slice(2)}`;
 	try {
 		const ret = await execa('./cli.js', [pkgName1, pkgName2, '--color'], {cwd: __dirname});
 		t.regex(ret.stdout, /is unavailable([\s\S]*)is available/);
 	} catch (err) {
-		t.ok(err)
+		t.ok(err);
 	}
 });
