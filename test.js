436d185eb8fca3473c59d5658af7e9fe5443f671
Jason Maurer <j@maur.co>
2016-04-07 00:31:41 -0600

    syntax fixes

--- test.js
+++ test.js
@@ -2,13 +2,28 @@ import test from 'ava';
 import execa from 'execa';
 
 test(async t => {
-  const pkgName = `asdasfgrgafadsgaf${Math.random().toString().slice(2)}`;
-  const ret = await execa('./cli.js', [pkgName, '--color'], {cwd: __dirname});
-  t.regex(ret.stdout, /is available/);
+	const pkgName = `asdasfgrgafadsgaf${Math.random().toString().slice(2)}`;
+	const ret = await execa('./cli.js', [pkgName, '--color'], {cwd: __dirname});
+	t.regex(ret.stdout, /is available/);
 });
 
 test(async t => {
-  const pkgName = `chalk,asdasfgrgafadsgaf${Math.random().toString().slice(2)}`;
-  const ret = await execa('./cli.js', [pkgName, '--color'], {cwd: __dirname});
-  t.regex(ret.stdout, /is unavailable([\s\S]*)is available/);
+	const pkgName = 'chalk';
+	try {
+		const ret = await execa('./cli.js', [pkgName, '--color'], {cwd: __dirname});
+		t.regex(ret.stdout, /is unavailable/);
+	} catch (err) {
+		t.ok(err)
+	}
+});
+
+test(async t => {
+	const pkgName1 = 'chalk'
+	const pkgName2 = `asdasfgrgafadsgaf${Math.random().toString().slice(2)}`
+	try {
+		const ret = await execa('./cli.js', [pkgName1, pkgName2, '--color'], {cwd: __dirname});
+		t.regex(ret.stdout, /is unavailable([\s\S]*)is available/);
+	} catch (err) {
+		t.ok(err)
+	}
 });
